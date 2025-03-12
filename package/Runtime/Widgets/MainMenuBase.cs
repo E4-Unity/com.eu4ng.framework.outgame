29ad9f6f9537ecad166c9f257fdca9da3080136b
이정훈 <eu4ng97@gmail.com>
2025-03-12 19:28:21 +0900

    Feat: MainMenuBase 클래스에 버튼 이벤트 바인딩 기능 추가

--- package/Runtime/Widgets/MainMenuBase.cs
+++ package/Runtime/Widgets/MainMenuBase.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Eu4ng.Framework.OutGame
 {
@@ -9,10 +10,30 @@ namespace Eu4ng.Framework.OutGame
     /// </summary>
     public class MainMenuBase : UserWidget
     {
+        [Header("References")]
+        [SerializeField] protected Button m_PlayButton;
+        [SerializeField] protected Button m_OptionsButton;
+        [SerializeField] protected Button m_ExitButton;
+
+        [Header("Config")]
         [SerializeField] protected bool m_UseSceneIndex = true;
         [SerializeField] protected int m_MainSceneIndex = 1;
         [SerializeField] protected string m_MainSceneName = "Main";
         [SerializeField] protected RectTransform m_OptionsWidget;
+
+        /* MonoBehaviour */
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            // Bind button events
+            if (m_PlayButton != null) m_PlayButton.onClick.AddListener(OnPlayButtonClicked);
+            if (m_OptionsButton != null) m_OptionsButton.onClick.AddListener(OnOptionsButtonClicked);
+            if (m_ExitButton != null) m_ExitButton.onClick.AddListener(OnExitButtonClicked);
+        }
+
+        /* MainMenuBase */
 
         public virtual void OnPlayButtonClicked()
         {
