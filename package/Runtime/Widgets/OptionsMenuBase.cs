c92ac1c2dc786c67bbf031df0787f56a25b0c303
이정훈 <eu4ng97@gmail.com>
2025-03-12 19:31:56 +0900

    Feat: OptionsMenuBase 클래스 작성

---
+++ package/Runtime/Widgets/OptionsMenuBase.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Eu4ng.Framework.OutGame
+{
+    public class OptionsMenuBase : UserWidget
+    {
+        [Header("References")]
+        [SerializeField] protected Button m_ApplyButton;
+        [SerializeField] protected Button m_ConfirmButton;
+
+        /* MonoBehaviour */
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if(m_ApplyButton != null) m_ApplyButton.onClick.AddListener(OnApplyButtonClicked);
+            if(m_ConfirmButton != null) m_ConfirmButton.onClick.AddListener(OnConfirmButtonClicked);
+        }
+
+        /* OptionsMenuBase */
+
+        protected virtual void OnApplyButtonClicked()
+        {
+            Debug.Log("Apply Button Clicked");
+        }
+
+        protected virtual void OnConfirmButtonClicked()
+        {
+            Debug.Log("Confirm Button Clicked");
+        }
+    }
+}
