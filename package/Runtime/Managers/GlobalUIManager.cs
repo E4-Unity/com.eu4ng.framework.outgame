4652d597c374f18af816f2e08d9580c553d55e7e
이정훈 <eu4ng97@gmail.com>
2025-03-12 18:50:08 +0900

    Feat: UIManagerBase, GlobalUIManager 클래스 작성

---
+++ package/Runtime/Managers/GlobalUIManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Eu4ng.Framework.OutGame
+{
+    /// <summary>
+    /// 모든 씬에서 사용할 수 있는 공용 UIManager 싱글톤 클래스입니다.
+    /// Alert, Confirm, Prompt 등의 모달 창을 띄울 수 있습니다.
+    /// </summary>
+    public class GlobalUIManager : UIManagerBase<GlobalUIManager>
+    {
+        
+    }
+}
