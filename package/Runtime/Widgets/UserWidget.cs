50bd2e68a7ca2b6efec3b51bdebb06dfd76c9b62
이정훈 <eu4ng97@gmail.com>
2025-03-12 18:05:05 +0900

    Feat: UserWidget 클래스에 IUIManager 인터페이스 구현

--- package/Runtime/Widgets/UserWidget.cs
+++ package/Runtime/Widgets/UserWidget.cs
@@ -5,8 +5,25 @@ namespace Eu4ng.Framework.OutGame
     /// <summary>
     /// 기본 위젯 클래스로 UIManager 클래스와 연동되어 있습니다.
     /// </summary>
-    public abstract class UserWidget : MonoBehaviour
+    public abstract class UserWidget : MonoBehaviour, IUIManager
     {
+        /* UserWidget */
+
+        protected virtual IUIManager UIManagerInterface => UIManager.Instance;
+
+        /* IUIManager */
+
+        public void AddWidget(RectTransform widgetPrefab) => UIManagerInterface.AddWidget(widgetPrefab);
+
+        public void RemoveWidget(RectTransform widgetPrefab) => UIManagerInterface.RemoveWidget(widgetPrefab);
+
+        public void RemoveAllWidgets() => UIManagerInterface.RemoveAllWidgets();
+
+        public void ShowWidget(RectTransform widgetPrefab) => UIManagerInterface.ShowWidget(widgetPrefab);
+
+        public void HideWidget(RectTransform widgetPrefab) => UIManagerInterface.HideWidget(widgetPrefab);
+
+        /* MonoBehaviour */
         protected virtual void Awake() {}
 
         protected virtual void OnEnable() {}
