bf3940cfb16d096b096a056fd26db08c4e80c447
이정훈 <eu4ng97@gmail.com>
2025-03-16 21:05:49 +0900

    Feat: OutGameFrameworkSettings 생성

---
+++ package/Runtime/OutGameFrameworkSettings.cs
@@ -0,0 +1,15 @@
+using Eu4ng.Manager.Singleton;
+using UnityEngine;
+
+namespace Eu4ng.Framework.OutGame
+{
+    /// <summary>
+    /// OutGameFramework 패키지 전용 설정
+    /// </summary>
+    public class OutGameFrameworkSettings : DeveloperSettings<OutGameFrameworkSettings>
+    {
+        /* DeveloperSettings */
+
+        protected override void OnInitialize() {}
+    }
+}
