e6c3ce8c91de0fe15b07b3fe6c885ca0c4c4bebf
이정훈 <eu4ng97@gmail.com>
2025-03-17 23:05:01 +0900

    Feat: SceneWidgetConfig 클래스 작성

--- package/Runtime/OutGameFrameworkSettings.cs
+++ package/Runtime/OutGameFrameworkSettings.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Eu4ng.Manager.Singleton;
 using UnityEngine;
 
@@ -13,12 +14,31 @@ namespace Eu4ng.Framework.OutGame
         [Header("Config")]
         [SerializeField] RectTransform m_OptionsWidget;
 
+        [Header("UI Manager")]
+        [SerializeReference] List<SceneWidgetsConfig> m_SceneWidgetsConfigList;
+
+        Dictionary<int, List<RectTransform>> m_SceneWidgetsDictionary;
+
         /* Properties */
 
         public RectTransform OptionsWidget => m_OptionsWidget;
 
+        /* OutGameFrameworkSettings */
+
+        public List<RectTransform> GetStartupWidgets(int buildIndex) =>
+            m_SceneWidgetsDictionary.GetValueOrDefault(buildIndex, new List<RectTransform>());
+
         /* DeveloperSettings */
 
-        protected override void OnInitialize() {}
+        protected override void OnInitialize()
+        {
+            foreach (var sceneWidgetsConfig in m_SceneWidgetsConfigList)
+            {
+                if (sceneWidgetsConfig != null)
+                {
+                    m_SceneWidgetsDictionary.TryAdd(sceneWidgetsConfig.BuildIndex, sceneWidgetsConfig.StartupWidgets);
+                }
+            }
+        }
     }
 }
