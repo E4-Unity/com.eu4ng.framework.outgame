67544fd3f00d3808ab4ec97ec5907ed829eaa419
이정훈 <eu4ng97@gmail.com>
2025-03-18 02:01:06 +0900

    Feat: SceneWidgetsManager 클래스 작성

--- package/Runtime/OutGameFrameworkSettings.cs
+++ package/Runtime/OutGameFrameworkSettings.cs
@@ -14,27 +14,29 @@ namespace Eu4ng.Framework.OutGame
         [Header("Config")]
         [SerializeField] RectTransform m_OptionsWidget;
 
-        [Header("UI Manager")]
-        [SerializeReference] List<SceneWidgetsConfig> m_SceneWidgetsConfigList;
-        Dictionary<int, List<RectTransform>> m_SceneWidgetsDictionary;
-
-        [Header("Modal")]
+        [Header("Modal Manager")]
         [SerializeField] RectTransform m_AlertWidgetPrefab;
         [SerializeField] RectTransform m_ConfirmWidgetPrefab;
         [SerializeField] RectTransform m_PromptWidgetPrefab;
+
+        [Header("Scene Widgets Manager")]
+        [SerializeField] List<RectTransform> m_GlobalWidgetPrefabs;
+        [SerializeReference] List<SceneWidgetsConfig> m_SceneWidgetsConfigList;
+
+        Dictionary<int, List<RectTransform>> m_SceneWidgetsDictionary;
 
         /* Properties */
 
         public RectTransform OptionsWidget => m_OptionsWidget;
 
+        // Modal Manager
         public RectTransform AlertWidgetPrefab => m_AlertWidgetPrefab;
         public RectTransform ConfirmWidgetPrefab => m_ConfirmWidgetPrefab;
         public RectTransform PromptWidgetPrefab => m_PromptWidgetPrefab;
 
-        /* OutGameFrameworkSettings */
-
-        public List<RectTransform> GetStartupWidgets(int buildIndex) =>
-            m_SceneWidgetsDictionary.GetValueOrDefault(buildIndex, new List<RectTransform>());
+        // Scene Widgets Manager
+        public List<RectTransform> GlobalWidgetPrefabs => m_GlobalWidgetPrefabs;
+        public List<RectTransform> GetStartupWidgets(int buildIndex) => m_SceneWidgetsDictionary.GetValueOrDefault(buildIndex, new List<RectTransform>());
 
         /* DeveloperSettings */
 
---
+++ package/Runtime/UIManager/SceneWidgetsManager.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using Eu4ng.Utilities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Eu4ng.Framework.OutGame
+{
+    public class SceneWidgetsManager : MonoBehaviour
+    {
+        [Header("State")]
+        [SerializeField, ReadOnly] List<RectTransform> m_GlobalWidgets = new List<RectTransform>();
+        [SerializeField, ReadOnly] List<RectTransform> m_SceneWidgets = new List<RectTransform>();
+
+        OutGameFrameworkSettings Settings => OutGameFrameworkSettings.Instance;
+        IUIManager UIManagerInterface => GlobalUIManager.Instance;
+
+        protected virtual void Awake()
+        {
+            CreateGlobalWidgets();
+
+            SceneManager.activeSceneChanged += OnActiveSceneChanged;
+        }
+
+        protected virtual void CreateGlobalWidgets()
+        {
+            if (m_GlobalWidgets.Count > 0) return;
+
+            foreach (var globalWidgetPrefab in Settings.GlobalWidgetPrefabs)
+            {
+                UIManagerInterface.ShowWidget(globalWidgetPrefab);
+                var globalWidget = UIManagerInterface.GetWidgetInstance(globalWidgetPrefab);
+                m_GlobalWidgets.Add(globalWidget);
+            }
+        }
+
+        protected virtual void OnActiveSceneChanged(Scene currentScene, Scene nextScene)
+        {
+            DestroySceneWidgets();
+
+            CreateSceneWidgets(nextScene.buildIndex);
+        }
+
+        protected virtual void DestroySceneWidgets()
+        {
+            foreach (var sceneWidget in m_SceneWidgets)
+            {
+                IUserWidget userWidgetInterface = sceneWidget.GetComponent<IUserWidget>();
+                userWidgetInterface?.Remove();
+            }
+
+            m_SceneWidgets.Clear();
+        }
+
+        protected virtual void CreateSceneWidgets(int buildIndex)
+        {
+            var sceneWidgetPrefabs = Settings.GetStartupWidgets(buildIndex);
+            foreach (var sceneWidgetPrefab in sceneWidgetPrefabs)
+            {
+                UIManagerInterface.ShowWidget(sceneWidgetPrefab);
+                var globalWidget = UIManagerInterface.GetWidgetInstance(sceneWidgetPrefab);
+                m_SceneWidgets.Add(globalWidget);
+            }
+        }
+    }
+}
