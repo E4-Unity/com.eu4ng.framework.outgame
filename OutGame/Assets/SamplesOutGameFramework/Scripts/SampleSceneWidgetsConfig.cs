be7de81f8f016697174cc7aadb2b4df8d3f3d3c4
이정훈 <eu4ng97@gmail.com>
2025-03-18 16:30:00 +0900

    Refactor: SampleSceneType 스크립트 분리

--- OutGame/Assets/SamplesOutGameFramework/Scripts/SampleSceneWidgetsConfig.cs
+++ OutGame/Assets/SamplesOutGameFramework/Scripts/SampleSceneWidgetsConfig.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 namespace Eu4ng.Framework.OutGame.Sample
 {
-    public enum SampleSceneType
-    {
-        MainMenu,
-        Main
-    }
-
     [CreateAssetMenu(fileName = "SampleSceneWidgetsConfig", menuName = "Scriptable Objects/OutGameFramework/SampleSceneWidgetsConfig")]
     public class SampleSceneWidgetsConfig : SceneWidgetsConfig<SampleSceneType>
     {
-        
+
     }
 }
