8d7463093762861814c3c1bcb49d52a9336dd00c
이정훈 <eu4ng97@gmail.com>
2025-03-28 16:46:32 +0900

    Feat: CloseButton 클래스 작성

---
+++ package/Runtime/Widgets/Button/CloseButton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Eu4ng.Framework.OutGame
+{
+    /// <summary>
+    /// 위젯 닫기 버튼
+    /// </summary>
+    public class CloseButton : ButtonWidget
+    {
+        [SerializeField] RectTransform m_WidgetPrefab;
+
+        protected RectTransform WidgetPrefab => m_WidgetPrefab;
+
+        protected override void OnButtonClicked()
+        {
+            base.OnButtonClicked();
+
+            if (WidgetPrefab != null) HideWidget(WidgetPrefab);
+        }
+    }
+}
