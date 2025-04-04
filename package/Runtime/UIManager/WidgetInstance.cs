f51de8cb5b8c065d64734728f6414b62b1218a68
이정훈 <eu4ng97@gmail.com>
2025-04-04 14:40:27 +0900

    Feat: WidgetInstance 클래스 작성

---
+++ package/Runtime/UIManager/WidgetInstance.cs
@@ -0,0 +1,23 @@
+using Eu4ng.Utilities;
+using UnityEngine;
+
+namespace Eu4ng.Framework.OutGame
+{
+    public class WidgetInstance : MonoBehaviour
+    {
+        [field: SerializeField, ReadOnly] public RectTransform WidgetPrefab { get; private set; }
+        [field: SerializeField, ReadOnly] public bool IsGlobalWidget { get; private set; }
+
+        public void Initialize(RectTransform widgetPrefab, bool isGlobalWidget)
+        {
+            if (widgetPrefab is null) return;
+            if (WidgetPrefab is not null) return;
+
+            WidgetPrefab = widgetPrefab;
+            IsGlobalWidget = isGlobalWidget;
+        }
+        public void Show() => UIManager.Instance.ShowWidget(WidgetPrefab);
+        public void Hide() => UIManager.Instance.HideWidget(WidgetPrefab);
+        public void Remove() => UIManager.Instance.RemoveWidget(WidgetPrefab);
+    }
+}
